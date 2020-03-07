ff13f7fc09b881b90fe4d7ebedaef3c42856526b
Ross Schlaikjer <ross@schlaikjer.net>
2020-03-07 17:28:33 -0500

    Don't index pins by PAD, it's not unique

--- lattice_to_kicad.py
+++ lattice_to_kicad.py
@@ -27,6 +27,9 @@ class LatticePad:
     def is_nc(self):
         return self.pin_ball == 'NC'
 
+    def ball_for_package(self, package):
+        return self.part_mapping[package]
+
     def __str__(self):
         return (
             "LatticePad: \n"
@@ -46,13 +49,13 @@ class LatticePad:
             self.differential,
             self.high_speed,
             self.dqs,
-            ",".join(self.part_mapping.keys())
+            ",".join(["%s:%s"% (k, v) for (k, v) in self.part_mapping.items()])
         )
 
 
 class LatticeCSV:
     def __init__(self, filename):
-        self._pads = {}
+        self._pads = []
 
         # Load raw CSV data
         with open(filename) as csvfile:
@@ -77,11 +80,11 @@ class LatticeCSV:
             else:
                 # We know the header, parse this row as data.
                 lattice_pad = LatticePad(_header, row)
-                self._pads[lattice_pad.pad_number] = lattice_pad
+                self._pads.append(lattice_pad)
 
     def get_signals_for_part(self, part):
         ret = []
-        for pad in self._pads.values():
+        for pad in self._pads:
             if part in pad.part_mapping.keys():
                 if pad.part_mapping[part] != '-':
                     ret.append(pad)
@@ -89,64 +92,148 @@ class LatticeCSV:
 
     def get_part_names(self):
         names = set()
-        for pad in self._pads.values():
+        for pad in self._pads:
             for part in pad.part_mapping.keys():
                 names.add(part)
         return names
 
 
-def csv_to_rows(filename):
-    with open(filename) as csvfile:
-        reader = csv.reader(csvfile)
-        header = next(reader)
-        return (header, [row for row in reader])
+class KicadPart:
 
-def pin_compare_wrapper(tup1, tup2):
-    return pin_compare(tup1[0], tup2[0])
+    def __init__(self, part, package):
+        self.part = part
+        self.package = package
+        self.banks = {}
 
-def cmp(x, y):
-    return (x > y) - (x < y)
+    def add_bank(self, bank_id, pads):
+        self.banks[bank_id] = KicadBank(self.package, bank_id, pads)
 
-def pin_compare(pin1, pin2):
-    # Find the common prefix to both pins
-    len1 = len(pin1)
-    len2 = len(pin2)
-    prefix = ""
-    i = 0
-    for i in range(min(len1, len2)):
-        if pin1[i] == pin2[i]:
-            prefix += pin1[i]
-        else:
-            break
+    def emit(self):
+        # Part header
+        part_name = '%s-%s' % (self.part, self.package)
+        print('DEF {partname} U 0 20 Y Y {units} L N'.format(
+            partname=part_name,
+            units=len(self.banks)
+        ))
+        print('F0 "U" 0 0 50 H V C CNN')
+        print('F1 "{partname}" 0 0 50 H V C CNN'.format(
+            partname=part_name
+        ))
+        print('F2 "" 0 0 50 H I C CNN')
+        print('F3 "" 0 0 50 H I C CNN')
+        print('DRAW')
 
-    # Remove the prefix part from both strings
-    pin1 = pin1[i:]
-    pin2 = pin2[i:]
+        # Generate the IO banks
+        for bank in self.banks.values():
+            bank.emit()
 
-    # Take the longest int prefix from both strings
-    is_numchar = lambda c: c >= '0' and c <= '9'
-    intprefix1 = ''.join(c for c in itertools.takewhile(is_numchar, list(pin1)))
-    intprefix2 = ''.join(c for c in itertools.takewhile(is_numchar, list(pin2)))
+        # Part footer
+        print("ENDDRAW")
+        print("ENDDEF")
 
-    # If the prefix is missing on one, compare classically
-    if not intprefix1 or not intprefix2:
-        # Not the sort of pin pattern we're handling,
-        # return a normal compare
-        return cmp(pin1, pin2)
 
-    intprefix1 = int(intprefix1)
-    intprefix2 = int(intprefix2)
+class KicadBank:
 
-    # If the numbers aren't equal, compare on these
-    if int(intprefix1) != int(intprefix2):
-        return cmp(intprefix1, intprefix2)
+    def __init__(self, package, bank_number, pads):
+        # Map of signal name to list of physical pads
+        self._pads = defaultdict(list)
+        self._bank_number = bank_number
+        # Stack all the pads with the same net
+        for pad in pads:
+            self._pads[pad.pin_ball].append(pad.ball_for_package(package))
 
-    # If the numbers were equal, compare based on the suffix
-    intsuffix1 = ''.join(c for c in itertools.dropwhile(is_numchar, pin1))
-    intsuffix2 = ''.join(c for c in itertools.dropwhile(is_numchar, pin2))
-    return cmp(intsuffix1, intsuffix2)
+    def emit(self):
+        # Get the total number of unique signals in this bank
+        signal_names = self._pads.keys()
 
-def make_rect(start_x, start_y, end_x, end_y, unit):
+        # Split the signals into Vcc, GND, and other
+        vcc_signals = [name for name in signal_names if is_vcc_pin(name)]
+        gnd_signals = [name for name in signal_names if is_gnd_pin(name)]
+        other_signals = [
+            name for name in signal_names
+            if (name not in vcc_signals) and (name not in gnd_signals)]
+        print("Bank %s:" % self._bank_number)
+        print("Signal names: %s" % self._pads.keys())
+        print("Vcc signals: %s" % vcc_signals)
+        print("Gnd signals: %s" % gnd_signals)
+        print("Other signals: %s" % other_signals)
+
+    @staticmethod
+    def pin_compare_wrapper(tup1, tup2):
+        return KicadBank.pin_compare(tup1[0], tup2[0])
+
+    @staticmethod
+    def cmp(x, y):
+        return (x > y) - (x < y)
+
+    @staticmethod
+    def pin_compare(pin1, pin2):
+        # Find the common prefix to both pins
+        len1 = len(pin1)
+        len2 = len(pin2)
+        prefix = ""
+        i = 0
+        for i in range(min(len1, len2)):
+            if pin1[i] == pin2[i]:
+                prefix += pin1[i]
+            else:
+                break
+
+        # Remove the prefix part from both strings
+        pin1 = pin1[i:]
+        pin2 = pin2[i:]
+
+        # Take the longest int prefix from both strings
+        is_numchar = lambda c: c >= '0' and c <= '9'
+        intprefix1 = ''.join(c for c in itertools.takewhile(is_numchar, list(pin1)))
+        intprefix2 = ''.join(c for c in itertools.takewhile(is_numchar, list(pin2)))
+
+        # If the prefix is missing on one, compare classically
+        if not intprefix1 or not intprefix2:
+            # Not the sort of pin pattern we're handling,
+            # return a normal compare
+            return KicadBank.cmp(pin1, pin2)
+
+        intprefix1 = int(intprefix1)
+        intprefix2 = int(intprefix2)
+
+        # If the numbers aren't equal, compare on these
+        if int(intprefix1) != int(intprefix2):
+            return KicadBank.cmp(intprefix1, intprefix2)
+
+        # If the numbers were equal, compare based on the suffix
+        intsuffix1 = ''.join(c for c in itertools.dropwhile(is_numchar, pin1))
+        intsuffix2 = ''.join(c for c in itertools.dropwhile(is_numchar, pin2))
+        return KicadBank.cmp(intsuffix1, intsuffix2)
+
+
+def split_pads_by_bank(pads):
+    pads_per_bank = defaultdict(list)
+    for pad in pads:
+        pads_per_bank[pad.bank].append(pad)
+    return pads_per_bank
+
+
+def generate_kicad_part(part, csv_data, package):
+    # Get all the pads for this part
+    print("Generating part for package '%s'" % package)
+    pads = csv_data.get_signals_for_part(package)
+
+    # First thing we're going to do is separate all of the pads in to I/O banks.
+    # Pins that don't specify a bank (-) in the CSV will be grouped into a final
+    # 'Power' bank.
+    pads_per_bank = split_pads_by_bank(pads)
+    print("Total IO banks: %s" % len(pads_per_bank.keys()))
+
+    # Wrap all the bank pads
+    kicad_part = KicadPart(part, package)
+    for bank, pads in pads_per_bank.items():
+        kicad_part.add_bank(bank, pads)
+
+    # Emit the kicad symbol data
+    kicad_part.emit()
+
+def kicad_make_rect(start_x, start_y, end_x, end_y, unit):
     # S X1 Y1 X2 Y2 part dmg pen fill
     return "S {x1} {y1} {x2} {y2} {unit} 1 0 N".format(
         x1=start_x,
@@ -156,7 +243,7 @@ def make_rect(start_x, start_y, end_x, end_y, unit):
         unit=unit,
     )
 
-def make_pin(signal, pad, unit, x, y, orientation='L'):
+def kicad_make_pin(signal, pad, unit, x, y, orientation='L'):
     # X name pin X Y length orientation sizenum sizename part dmg type shape
     # I(nput), O(utout), B(idirectional), T(ristate),
     # P(assive), (open) C(ollector), (open) E(mitter), N(on-connected),
@@ -191,7 +278,7 @@ def is_power_pin(pinname):
     if is_vcc_pin(pinname):
         return True
 
-def main():
+def old_main():
     if len(sys.argv) != 4:
         sys.stderr.write("Usage: %s lattice_csv part package\n" % sys.argv[0])
         return
@@ -231,20 +318,6 @@ def main():
 
     # Get number of banks
     banks = sorted(signals_per_bank.keys())
-
-    # Part def
-    part_name = '%s-%s' % (part, package)
-    print('DEF {partname} U 0 20 Y Y {units} L N'.format(
-        partname=part_name,
-        units=len(banks)
-    ))
-    print('F0 "U" 0 0 50 H V C CNN')
-    print('F1 "{partname}" 0 0 50 H V C CNN'.format(
-        partname=part_name
-    ))
-    print('F2 "" 0 0 50 H I C CNN')
-    print('F3 "" 0 0 50 H I C CNN')
-    print('DRAW')
 
     # Now, for each bank, sort and print the pins
     unit_number = 1
@@ -286,16 +359,17 @@ def main():
 
         unit_number += 1
 
-    # End part
-    print("ENDDRAW")
-    print("ENDDEF")
 
-def test_main():
-    csv_data = LatticeCSV(sys.argv[1])
-    signals = csv_data.get_signals_for_part("CABGA256")
-    for s in signals:
-        print(s)
+def main():
+    # Load the CSV data for the part
+    part_name = sys.argv[1]
+    csv_data = LatticeCSV(sys.argv[2])
+    # Get the list of possible packages
+    packages = csv_data.get_part_names()
+    # Generate KiCad symbols for each package variant
+    for package in packages:
+        generate_kicad_part(part_name, csv_data, package)
 
 
 if __name__ == '__main__':
-    test_main()
+    main()
